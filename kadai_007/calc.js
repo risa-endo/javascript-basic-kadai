05eff5d28cf1ad078ffaa8cc2631eac183a87c2f
risa-endo <167213224+risa-endo@users.noreply.github.com>
2024-06-02 17:20:58 +0900

    ファイルをアップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -3,20 +3,21 @@ let num = Math.floor(Math.random() * 15);
 console.log(num);
 // 表示確認
 
-if (num % 3 == 0) {
+if (num % 15 == 0) {
+  console.log('3と5の倍数です')
+}
+// (1)numを15で割ったらあまり0になる場合
+
+else if (num % 3 == 0) {
   console.log('3の倍数です');
 }
-// (1)_numを３で割ったらあまり０になる場合
+// (2)_(1)でなくnumを３で割ったらあまり０になる場合
 
 else if (num % 5 == 0) {
   console.log('5の倍数です');
 }
-// (2)_(1)でなく５で割ったらあまり0になる場合
+// (3)_(1)でなく５で割ったらあまり0になる場合
 
-else if (num % 15 == 0) {
-  console.log('3と5の倍数です')
-}
-// (3)_(1)でなくnumを15で割ったらあまり0になる場合
 
 else {
   console.log(num);
